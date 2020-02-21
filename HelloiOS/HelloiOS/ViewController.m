c9002eb4612d89ed86ddece97b062118a7ba9c4a
wenjiewu <wenjiewu.tencent.com>
2020-02-21 20:57:05 +0800

    了解UIView的生命周期.

--- HelloiOS/HelloiOS/ViewController.m
+++ HelloiOS/HelloiOS/ViewController.m
@@ -8,6 +8,35 @@
 
 #import "ViewController.h"
 
+@interface TestView : UIView
+
+@end
+
+@implementation TestView
+
+- (instancetype)init {
+    self = [super init];
+    return self;
+}
+
+- (void)willMoveToSuperview:(nullable UIView *)newSuperview {
+    [super willMoveToSuperview:newSuperview];
+}
+
+- (void)didMoveToSuperview {
+    [super didMoveToSuperview];
+}
+
+- (void)willMoveToWindow:(nullable UIWindow *)newWindow {
+    [super willMoveToWindow:newWindow];
+}
+
+- (void)didMoveToWindow {
+    [super didMoveToWindow];
+}
+
+@end
+
 @interface ViewController ()
 
 @end
@@ -24,6 +53,11 @@
         label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
         label;
     })];
+    
+    TestView *testView = [[TestView alloc] init];
+    testView.backgroundColor = [UIColor greenColor];
+    testView.frame = CGRectMake(150, 160, 100, 100);
+    [self.view addSubview:testView];
 }
 
 
